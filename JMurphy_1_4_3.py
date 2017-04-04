d11d03f4ad91c7dfdf2b61de6001f27890094691
AvrgeCupOfJoe <jomurp17@nsd.newberg.k12.or.us>
2017-04-04 14:34:37 -0700

    Added 1.4.3 and make_mask

---
+++ JMurphy_1_4_3.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+'''
+JMurphy_1_4_3: Change pixels in an image.
+'''
+import matplotlib.pyplot as plt 
+import os.path
+import numpy as np      # “as” lets us use standard abbreviations
+
+'''Read the image data'''
+# Get the directory of this python script
+directory = os.path.dirname(os.path.abspath(__file__)) 
+# Build an absolute filename from directory + filename
+filename = os.path.join(directory, 'woman.jpg')
+# Read the image data into an array
+img = plt.imread(filename)
+
+'''Show the image data'''
+# Create figure with 1 subplot
+fig, ax = plt.subplots(1, 1)
+# Show the image data in a subplot
+ax.imshow(img, interpolation='none')
+# Show the figure on the screen
+
+
+###
+# Change a region if condition is True
+###
+
+height = len(img)
+width = len(img[0])
+for r in range(155):
+    for c in range(width):
+        if sum(img[r][c])>500: # brightness R+G+B goes up to 3*255=765
+            img[r][c]=[255,0,255] # R + B = magenta
+for r in range(418, 475):
+    for c in range(125, 160):
+        if sum(img[r][c])>500: # brightness R+G+B goes up to 3*255=765
+            img[r][c]=[0 ,255, 0] # R + B = magenta
+###
+# Show the image data
+###
+fig.show()
